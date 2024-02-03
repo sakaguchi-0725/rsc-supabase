76b7aee3baae2b5e807d47f7fe13659a5147cbba
sakaguchi <kzmsan63@gmail.com>
2024-02-03 12:18:55 +0900

    create notes-list

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import NotesList from "./components/notes-list"
+
 export default function Page() {
   return (
     <main>
-      <div className="m-10 text-center">Hello World🚀</div>
+      <div className="m-10 text-center">
+        <p>Hello World🚀</p>
+        <NotesList />
+      </div>
     </main>
   )
 }
