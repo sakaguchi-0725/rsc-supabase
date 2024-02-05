955f5fd22dc92f4833749343bd9d67a0cc9503a6
sakaguchi <kzmsan63@gmail.com>
2024-02-05 21:28:56 +0900

    timer-counterコンポーネント追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+import { Suspense } from "react"
 import NotesList from "./components/notes-list"
+import TimerCounter from "./components/timer-counter"
+import Spinner from "./components/spinner"
+import RefreshBtn from "./components/refresh-btn"
 
 export default function Page() {
   return (
     <main>
       <div className="m-10 text-center">
         <p>Hello World🚀</p>
-        <NotesList />
+        <Suspense fallback={<Spinner color="border-green-500" />}>
+          <NotesList />
+        </Suspense>
+        <TimerCounter />
+        <RefreshBtn />
       </div>
     </main>
   )
